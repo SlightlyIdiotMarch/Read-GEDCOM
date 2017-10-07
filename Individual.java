37924e7ef067a3860186c74eb03cbf9c19775955
euloanty <euloanty@live.com>
2017-10-07 02:14:22 -0400

    fix exception neutral and safety, resources leak issue

--- Individual.java
+++ Individual.java
@@ -1,5 +1,3 @@
-package cs555;
-
 import java.awt.List;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -8,45 +6,50 @@ import java.util.ArrayList;
 
 public class Individual {
 	public static void main(String[] args) {
-		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\shizekang\\Desktop\\cs-555\\project1.ged"));
-			String string;
-			ArrayList<String> all = new ArrayList<String>();
-			while ((string = bufferedReader.readLine()) != null) { // save all information to the list
-				all.add(string);
-			}
-			ArrayList<String> individual = new ArrayList<String>();
- 			for (int i = 0; i < all.size();) { // create individuals collection
-				if (all.get(i).split(" ").length > 2 && all.get(i).split(" ")[2].equals("INDI")) {
-					String ss = all.get(i).split(" ")[1];
-					while (i < all.size()) {
-						if (all.get(i).split(" ").length > 2 && all.get(i).split(" ")[1].equals("NAME")) {
-							ss += all.get(i).substring(6, all.get(i).length());
-							individual.add(ss);
-							break;
+		try
+		{
+			if(args.length==0)
+				throw new RuntimeException("Usage : <Filename>");
+			try(FileReader fr = new FileReader(args[0]);
+				BufferedReader bufferedReader = new BufferedReader(fr))
+			{
+				String string;
+				ArrayList<String> all = new ArrayList<String>();
+				while ((string = bufferedReader.readLine()) != null) { // save all information to the list
+					all.add(string);
+				}
+				ArrayList<String> individual = new ArrayList<String>();
+				for (int i = 0; i < all.size();) { // create individuals collection
+					if (all.get(i).split(" ").length > 2 && all.get(i).split(" ")[2].equals("INDI")) {
+						String ss = all.get(i).split(" ")[1];
+						while (i < all.size()) {
+							if (all.get(i).split(" ").length > 2 && all.get(i).split(" ")[1].equals("NAME")) {
+								ss += all.get(i).substring(6, all.get(i).length());
+								individual.add(ss);
+								break;
+							}
+							i++;
 						}
-						i++;
+					}
+					i++;
+				}
+				for (int i = 0; i < individual.size() - 1; ++i) { // sort
+					for (int j = i + 1; j < individual.size(); ++j) {
+						if (Integer.parseInt(individual.get(i).split(" ")[0].substring(1)) > Integer.parseInt(individual.get(j).split(" ")[0].substring(1))) {
+							String replace = individual.get(i);
+							individual.set(i, individual.get(j));
+							individual.set(j, replace);
+						}
 					}
 				}
-				i++;
+				for (String s: individual) {
+					System.out.println(s);
+				}
+				
 			}
- 			for (int i = 0; i < individual.size() - 1; i++) { // sort
- 				for (int j = i + 1; j < individual.size(); j++) {
- 					if (Integer.parseInt(individual.get(i).split(" ")[0].substring(1)) > Integer.parseInt(individual.get(j).split(" ")[0].substring(1))) {
- 						String replace = individual.get(i);
- 						individual.set(i, individual.get(j));
- 						individual.set(j, replace);
- 					}
- 				}
- 			}
- 			for (String s: individual) {
- 				System.out.println(s);
- 			}
- 			
-			bufferedReader.close();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
